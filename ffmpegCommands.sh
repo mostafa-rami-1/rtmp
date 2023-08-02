93a2d19ed251f9bb0a9b540208234c27c6c72b35
mostafa-rami-1 <maroca.rami003@gmail.com>
2023-08-02 18:26:43 +0100

    ffmpeg integration

---
+++ ffmpegCommands.sh
@@ -0,0 +1,3 @@
+-i 'http://line.rs4ott.com:80/live/609b58153b/19a2f5ea6c5b/554021.m3u8' -c:v copy -c:a aac -f flv rtmp://172.23.32.1:1935/bein1/hp?key=mostafa &
+
+-i 'http://line.rs4ott.com:80/live/609b58153b/19a2f5ea6c5b/554022.m3u8' -c:v copy -c:a aac -f flv rtmp://172.23.32.1:1935/bein2/hp?key=mostafa 
